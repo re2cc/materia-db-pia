5848362caa5626c12283a2d85b69d8662f12cc10
re2cc <42478705+re2cc@users.noreply.github.com>
2023-11-13 20:32:29 -0600

    Modifica los stores procedures y agrega algunos

--- SQLQueryEventosStoredProcedures.sql
+++ SQLQueryEventosStoredProcedures.sql
@@ -8,15 +8,35 @@ CREATE PROCEDURE requestLogin(
     @matricula int,
     @contraseña int
 ) AS BEGIN
-    IF EXISTS(SELECT usuarios.nombres, usuarios.apellidoPaterno, usuarios.apellidoMaterno FROM usuarios where @matricula = matricula and @contraseña = contraseña)
-    SELECT usuarios.nombres, usuarios.apellidoPaterno, usuarios.apellidoMaterno FROM usuarios where @matricula = matricula and @contraseña = contraseña
-    SELECT boletos.codigoBarras, eventos.nombre, eventos.fecha, dependencias.nombre FROM usuarios
-    INNER JOIN asistenciaUsuarios on usuarios.idUsuarios = asistenciaUsuarios.idUsuarios
-    INNER JOIN boletos on asistenciaUsuarios.idBoletos = boletos.idBoletos
-    INNER JOIN eventos on boletos.idEventos = eventos.idEventos
-    INNER JOIN dependencias on eventos.idDependencias = dependencias.idDependencias
-    WHERE @matricula = matricula
+    IF EXISTS(SELECT usuarios.idUsuarios FROM usuarios WHERE @matricula = matricula AND @contraseña = contraseña)
+    BEGIN
+        SELECT usuarios.nombres, usuarios.apellidoPaterno, usuarios.apellidoMaterno, boletos.codigoBarras,
+        eventos.nombre, eventos.fecha, dependencias.nombre FROM usuarios
+            INNER JOIN asistenciaUsuarios ON usuarios.idUsuarios = asistenciaUsuarios.idUsuarios
+            INNER JOIN boletos ON asistenciaUsuarios.idBoletos = boletos.idBoletos
+            INNER JOIN eventos ON boletos.idEventos = eventos.idEventos
+            INNER JOIN dependencias ON eventos.idDependencias = dependencias.idDependencias
+            WHERE @matricula = matricula
+    END
 end
 
-EXEC requestSalt 222
-EXEC requestLogin 222, 123
+CREATE PROCEDURE requestCrearInvitado(
+    @nombres nvarchar(50),
+    @apellidoPaterno nvarchar(50),
+    @apellidoMaterno nvarchar(50)
+) AS BEGIN
+    INSERT INTO invitados (nombres, apellidoPaterno, apellidoMaterno) VALUES (@nombres, @apellidoPaterno, @apellidoMaterno)
+end
+
+CREATE PROCEDURE requestEliminarBoletoUsuario(
+    @codigoBarras int
+) AS BEGIN
+    DELETE asistenciaUsuarios FROM asistenciaUsuarios INNER JOIN boletos
+        ON boletos.idBoletos = asistenciaUsuarios.idBoletos WHERE codigoBarras = @codigoBarras
+    DELETE FROM boletos WHERE codigoBarras = @codigoBarras
+end
+
+EXEC requestSalt 21212121
+EXEC requestLogin 21212121, 123
+EXEC requestCrearInvitado 'Setu', 'Re', 'Shou'
+EXEC requestEliminarBoletoUsuario 222222
