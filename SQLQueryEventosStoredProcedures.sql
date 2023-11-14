d958ad021747a30664cda5957d73e40d12e3e238
re2cc <42478705+re2cc@users.noreply.github.com>
2023-11-14 13:16:18 -0600

    Add updatePassword

--- SQLQueryEventosStoredProcedures.sql
+++ SQLQueryEventosStoredProcedures.sql
@@ -6,12 +6,12 @@ end
 
 CREATE PROCEDURE requestLogin(
     @matricula int,
-    @contraseña int
+    @contraseña nvarchar(60)
 ) AS BEGIN
     IF EXISTS(SELECT usuarios.idUsuarios FROM usuarios WHERE @matricula = matricula AND @contraseña = contraseña)
     BEGIN
         SELECT usuarios.nombres, usuarios.apellidoPaterno, usuarios.apellidoMaterno, boletos.codigoBarras,
-        eventos.nombre, eventos.fecha, dependencias.nombre FROM usuarios
+        asistenciaUsuarios.asistencia, eventos.nombre, eventos.fecha, dependencias.nombre FROM usuarios
             INNER JOIN asistenciaUsuarios ON usuarios.idUsuarios = asistenciaUsuarios.idUsuarios
             INNER JOIN boletos ON asistenciaUsuarios.idBoletos = boletos.idBoletos
             INNER JOIN eventos ON boletos.idEventos = eventos.idEventos
@@ -35,3 +35,15 @@ CREATE PROCEDURE requestEliminarBoletoUsuario(
         ON boletos.idBoletos = asistenciaUsuarios.idBoletos WHERE codigoBarras = @codigoBarras
     DELETE FROM boletos WHERE codigoBarras = @codigoBarras
 end
+
+CREATE PROCEDURE updatePassword(
+    @matricula int,
+    @contraseñaActual varchar(60),
+    @contraseñaNueva varchar(60),
+    @saltNuevo varchar(30)
+) AS BEGIN
+    UPDATE usuarios SET contraseña = @contraseñaNueva, salt = @saltNuevo
+        WHERE matricula = @matricula AND contraseña = @contraseñaActual
+end
+
+EXEC updatePassword 100, juan, juana, aaa
